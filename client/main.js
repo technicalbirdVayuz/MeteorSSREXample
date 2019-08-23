1d730ebd7b06a142778a0445b137346d803f080c
mixomatric <ankit.vayuz@gmail.com>
2019-08-23 16:15:02 +0530

    Server side rendring implementation

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
 
 
-import './main.html';
+// import './main.html';
 
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
